7097e9c67a9249cf9eea54125ebe9d2e59c52572
Josh North <josh.north@point808.com>
2015-02-09 11:51:21 -0500

    icons, default settings

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # yaptc
 Yet Another PHP Time Card
+
+
+# Credits:
+Default icon pack - Oliver Charavel "Sekkyumu" at http://sekkyumu.deviantart.com/ (http://sekkyumu.deviantart.com/art/Developpers-Icons-63052312)
+
+
---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+########## MySql details (Replace with yours) #############
+$username = "root"; //mysql username
+$password = "AdmiN!04%sui"; //mysql password
+$hostname = "localhost"; //hostname
+$databasename = "yaptc"; //databasename
+
+//connect to database
+$mysqli = new mysqli($hostname, $username, $password, $databasename);
+
+?>
