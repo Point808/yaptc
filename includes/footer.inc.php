9397a0f16940a2e0ec658debe091208686331f7b
Josh North <josh.north@point808.com>
2017-05-12 10:42:22 -0400

    fix stupid footer workaround on mobile.

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,11 +1,18 @@
                 </div>
             </div>
         </div>
+<!-- stupid footer doesn't stick on mobile.  screw it.
         <footer class="footer">
             <div class="container">
                 <p class="text-muted"><?php echo lang('SERVER_TIME') . ": " . $timenow . " - - - " . lang('SOFTWARE_VERSION') . ": " . $yaptc_version; ?></p>
             </div>
         </footer>
+-->
+<!-- here's a temporary alternative for a footer -->
+    <div class="container">
+      <p class="text-muted"><?php echo lang('SERVER_TIME') . ": " . $timenow . " - - - " . lang('SOFTWARE_VERSION') . ": " . $yaptc_version; ?></p>
+    </div>
+
         <script src="js/ie10-viewport-bug-workaround.js"></script>
         <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
         <script src="js/bootstrap.min.js"></script>
